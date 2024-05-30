1e232d68f7bd742faed2122138252f30ce7b5fbd
neha <neha@aruns-mbp>
2024-05-29 18:20:33 -0700

    d

--- src/_01_double/_2_test_scores/TestScores.java
+++ src/_01_double/_2_test_scores/TestScores.java
@@ -1,9 +1,22 @@
 package _01_double._2_test_scores;
 
+import javax.swing.JOptionPane;
+
 public class TestScores {
 
 	public static void main(String[] args){
-		A
+		
+		String testScore = JOptionPane.showInputDialog("Enter your test score.");
+		double score = Double.parseDouble(testScore);
+		if (score >= 85.0) {
+			JOptionPane.showMessageDialog(null, "Wow! Thats a great score.");
+		}else if (score < 85.0) {
+			JOptionPane.showMessageDialog(null, "Keep practicing.");
+		}
+		
+		
+		
+		
+		
 	}
 }
-ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
